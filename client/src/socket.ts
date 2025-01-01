5393da8f547e41a27e7e1f10b01f91c0d1644919
Joao Coelho <joaocoelho.tdm@gmail.com>
2025-01-01 17:10:02 -0300

    fix(package): correct formatting in package.json dependencies

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -1,4 +1,5 @@
 import { io } from "socket.io-client";
+import process from 'process';
 
 const URL = process.env.NODE_ENV === 'production' ? 'https://imagem-acao-v2.onrender.com' : 'http://localhost:3000';
 
