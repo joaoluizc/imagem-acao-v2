597f32840062852ddf3dbe868e4104184bd87954
Joao Coelho <joaocoelho.tdm@gmail.com>
2025-01-03 13:06:36 -0300

    fix(socket): update socket connection URL for production environment

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
-import process from 'process';
 
-const URL = process.env.NODE_ENV === 'production' ? 'https://imagem-acao-v2.onrender.com' : 'http://localhost:3000';
+const URL = 'https://imagem-acao-v2.onrender.com';
+// const URL = 'http://localhost:3000';
 
 export const socket = io(URL);
